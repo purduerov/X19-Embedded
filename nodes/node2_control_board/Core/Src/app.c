2c347f124ee523885efe9c882693528e0a9fe92e
Rex <wu2273@Purdue.edu>
2026-09-20 13:44:27 -0400

    style: clang-format node2 app

--- nodes/node2_control_board/Core/Src/app.c
+++ nodes/node2_control_board/Core/Src/app.c
@@ -21,12 +21,7 @@
 
 #define ESC_ARMING_TIME_MS 3000U
 
-typedef enum {
-    ESC_STATE_BOOT,
-    ESC_STATE_ARMING,
-    ESC_STATE_ACTIVE,
-    ESC_STATE_DISARMED
-} esc_state_t;
+typedef enum { ESC_STATE_BOOT, ESC_STATE_ARMING, ESC_STATE_ACTIVE, ESC_STATE_DISARMED } esc_state_t;
 
 static esc_state_t g_esc_state = ESC_STATE_BOOT;
 static uint32_t g_esc_arming_start_ms = 0U;
@@ -66,8 +61,7 @@ void node2_app_init(void) {
 void node2_app_step(void) {
     uint32_t current_time = time_get_ms();
 
-    if (g_esc_state == ESC_STATE_ARMING && (uint32_t)(current_time - g_esc_arming_start_ms) >= ESC_ARMING_TIME_MS)
-    {
+    if (g_esc_state == ESC_STATE_ARMING && (uint32_t)(current_time - g_esc_arming_start_ms) >= ESC_ARMING_TIME_MS) {
         g_esc_state = ESC_STATE_ACTIVE;
         g_last_ramp_time = current_time;
     }
@@ -93,40 +87,32 @@ void node2_app_step(void) {
                 }
             }
         } else if (rx_id == ROV_CAN_ID_THRUSTER_CMD) {
-    /* Thruster commands are accepted only after ESC arming completes */
-    if ((g_esc_state == ESC_STATE_ACTIVE) && (!g_safety_state.emergency_break_active))
-    {
-        rov_thruster_cmd_t cmd;
+            /* Thruster commands are accepted only after ESC arming completes */
+            if ((g_esc_state == ESC_STATE_ACTIVE) && (!g_safety_state.emergency_break_active)) {
+                rov_thruster_cmd_t cmd;
 
-        if (rov_can_unpack_thruster_cmd(rx_data, rx_len, &cmd) == ROV_OK)
-        {
-            rov_safety_feed_heartbeat(&g_safety_state, current_time);
+                if (rov_can_unpack_thruster_cmd(rx_data, rx_len, &cmd) == ROV_OK) {
+                    rov_safety_feed_heartbeat(&g_safety_state, current_time);
 
-            for (int i = 0; i < ROV_NUM_THRUSTERS; i++)
-            {
-                uint16_t target_us = cmd.pwm_us[i];
+                    for (int i = 0; i < ROV_NUM_THRUSTERS; i++) {
+                        uint16_t target_us = cmd.pwm_us[i];
 
-                if (target_us < 1000U)
-                {
-                    target_us = 1000U;
+                        if (target_us < 1000U) {
+                            target_us = 1000U;
+                        } else if (target_us > 2000U) {
+                            target_us = 2000U;
+                        }
+
+                        g_target_pwms.pwm_us[i] = target_us;
+                    }
                 }
-                else if (target_us > 2000U)
-                {
-                    target_us = 2000U;
-                }
-
-                g_target_pwms.pwm_us[i] = target_us;
+            }
+        } else if (rx_id == ROV_CAN_ID_SOLENOID_CMD) {
+            rov_solenoid_cmd_t sol;
+            if (rov_can_unpack_solenoid_cmd(rx_data, rx_len, &sol) == ROV_OK) {
+                bsp_solenoid_set(sol.solenoid_mask);
             }
         }
-    }   
-} else if (rx_id == ROV_CAN_ID_SOLENOID_CMD) 
-    {
-        rov_solenoid_cmd_t sol;
-        if (rov_can_unpack_solenoid_cmd(rx_data, rx_len, &sol) == ROV_OK) 
-        {
-            bsp_solenoid_set(sol.solenoid_mask);
-        }
-    }
     }
     /* Check heartbeat timeout: if no thruster command in 100 ms, drop to neutral */
     bool heartbeat_lost = rov_safety_is_heartbeat_lost(&g_safety_state, current_time);
@@ -137,21 +123,16 @@ void node2_app_step(void) {
     }
 
     /* Hold all ESCs at neutral throughout the mandatory arming period */
-    if (g_esc_state == ESC_STATE_ARMING)
-    {
-        for (int i = 0; i < ROV_NUM_THRUSTERS; i++)
-        {
+    if (g_esc_state == ESC_STATE_ARMING) {
+        for (int i = 0; i < ROV_NUM_THRUSTERS; i++) {
             g_target_pwms.pwm_us[i] = ROV_PWM_STOP_US;
             g_active_pwms.pwm_us[i] = ROV_PWM_STOP_US;
             bsp_pwm_set_us((uint8_t)i, ROV_PWM_STOP_US);
         }
 
         g_last_ramp_time = current_time;
-    }
-    else if (g_esc_state == ESC_STATE_DISARMED)
-    {
-        for (int i = 0; i < ROV_NUM_THRUSTERS; i++)
-        {
+    } else if (g_esc_state == ESC_STATE_DISARMED) {
+        for (int i = 0; i < ROV_NUM_THRUSTERS; i++) {
             g_target_pwms.pwm_us[i] = ROV_PWM_STOP_US;
             g_active_pwms.pwm_us[i] = ROV_PWM_STOP_US;
             bsp_pwm_set_us((uint8_t)i, ROV_PWM_STOP_US);
@@ -159,8 +140,7 @@ void node2_app_step(void) {
         g_last_ramp_time = current_time;
     }
     /* 1 kHz Slew-Rate Ramping Step (executed every 1 ms or on step) */
-    else if (current_time > g_last_ramp_time)
-    {
+    else if (current_time > g_last_ramp_time) {
         uint32_t dt_ms = current_time - g_last_ramp_time;
         g_last_ramp_time = current_time;
 
