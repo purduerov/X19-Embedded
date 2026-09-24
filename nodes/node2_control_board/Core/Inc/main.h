e50cedb6da8485580afe311bed2076d478b3b9ad
Rex <wu2273@Purdue.edu>
2026-09-23 20:22:28 -0400

    style(node2): format CubeMX generated sources

--- nodes/node2_control_board/Core/Inc/main.h
+++ nodes/node2_control_board/Core/Inc/main.h
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.h
-  * @brief          : Header for main.c file.
-  *                   This file contains the common defines of the application.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.h
+ * @brief          : Header for main.c file.
+ *                   This file contains the common defines of the application.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2026 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
@@ -61,22 +61,22 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define RCC_OSC32_IN_Pin GPIO_PIN_14
-#define RCC_OSC32_IN_GPIO_Port GPIOC
-#define RCC_OSC32_OUT_Pin GPIO_PIN_15
+#define RCC_OSC32_IN_Pin        GPIO_PIN_14
+#define RCC_OSC32_IN_GPIO_Port  GPIOC
+#define RCC_OSC32_OUT_Pin       GPIO_PIN_15
 #define RCC_OSC32_OUT_GPIO_Port GPIOC
-#define RCC_OSC_IN_Pin GPIO_PIN_0
-#define RCC_OSC_IN_GPIO_Port GPIOF
-#define RCC_OSC_OUT_Pin GPIO_PIN_1
-#define RCC_OSC_OUT_GPIO_Port GPIOF
-#define BMI270_CS_Pin GPIO_PIN_4
-#define BMI270_CS_GPIO_Port GPIOA
-#define T_SWDIO_Pin GPIO_PIN_13
-#define T_SWDIO_GPIO_Port GPIOA
-#define T_SWCLK_Pin GPIO_PIN_14
-#define T_SWCLK_GPIO_Port GPIOA
-#define T_SWO_Pin GPIO_PIN_3
-#define T_SWO_GPIO_Port GPIOB
+#define RCC_OSC_IN_Pin          GPIO_PIN_0
+#define RCC_OSC_IN_GPIO_Port    GPIOF
+#define RCC_OSC_OUT_Pin         GPIO_PIN_1
+#define RCC_OSC_OUT_GPIO_Port   GPIOF
+#define BMI270_CS_Pin           GPIO_PIN_4
+#define BMI270_CS_GPIO_Port     GPIOA
+#define T_SWDIO_Pin             GPIO_PIN_13
+#define T_SWDIO_GPIO_Port       GPIOA
+#define T_SWCLK_Pin             GPIO_PIN_14
+#define T_SWCLK_GPIO_Port       GPIOA
+#define T_SWO_Pin               GPIO_PIN_3
+#define T_SWO_GPIO_Port         GPIOB
 
 /* USER CODE BEGIN Private defines */
 
